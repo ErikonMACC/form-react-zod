bb5044df654520e077229719ba1322d9ff9371a2
ErikonMACC <erik.roblox0602@gmail.com>
2024-08-07 20:07:29 -0300

    Fixed a bug

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-    new : HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'head',
-      css: [
-        'index.css',
-        'Forms.css'
-      ]
-    })
+    autoprefixer: {}
   },
 }
